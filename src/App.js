fe95e9f8077cd89607421df35b40abfb8463c0cb
Wilson Chen <wilsonchen901@gmail.com>
2023-08-22 18:58:40 -0400

    profile layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import "./style.scss";
 import Header from "./components/Header";
+import About from "./components/About";
+import Profile from "./components/Profile";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Profile />
+      <About />
     </div>
   );
 };
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+
+const About = () => {
+  return (
+    <div className="about">
+        <div className="">
+
+        </div>
+    </div>
+  );
+};
+
+export default About;
