c1466308e9158931d94345b80b70ba5fc39c00d3
Wilson Chen <wilsonchen901@gmail.com>
2023-08-22 16:43:32 -0400

    scss reset

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
+import "./style.scss";
+import Header from "./components/Header";
 
 function App() {
   return (
     <div className="App">
-      
+      <Header />
     </div>
   );
-}
+};
 
 export default App;
