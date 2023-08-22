c7d8552a89cf5db52750107116525cff657b4097
Wilson Chen <wilsonchen901@gmail.com>
2023-08-22 16:52:41 -0400

    import roboto font

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,16 @@
 
 const Header = () => {
   return (
-    <div>
-      
+    <div className="header">
+      <div className="header-left">
+        wilsonchen.dev
+      </div>
+      <div className="header-right">
+        <span>Home</span>
+        <span>About</span>
+        <span>Projects</span>
+        <span>Contact</span>
+      </div>
     </div>
   );
 };
