cdd92a4de37ef6c448c6d114dfc4d280ed4d42b5
Wilson Chen <wilsonchen901@gmail.com>
2023-08-23 15:54:16 -0400

    hamburger menu on mobile screen size

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,15 +1,36 @@
+import { useEffect, useState } from "react";
+import hamburger from "../assets/hamburger.svg";
+import menu from "../assets/menu.svg";
 
 const Header = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+
+  }, []);
+
+  const showHamburger = windowWidth <= 975;
+
   return (
     <div className="header">
       <div className="header-left">
         wilsonchen.dev
       </div>
       <div className="header-right">
-        <span>Home</span>
-        <span>About</span>
-        <span>Projects</span>
-        <span>Contact</span>
+        {showHamburger && <img id='header-menu' src={hamburger} alt="menu"/>}
+        {!showHamburger && <span>Home</span>}
+        {!showHamburger && <span>About</span>}
+        {!showHamburger && <span>Projects</span>}
+        {!showHamburger && <span>Contact</span>}
       </div>
     </div>
   );
