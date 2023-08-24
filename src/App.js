540708a8e8f68b94b334865c73e1a66b48763b8a
Wilson Chen <wilsonchen901@gmail.com>
2023-08-24 15:16:24 -0400

    footer

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import About from "./components/About";
 import Profile from "./components/Profile";
 import Portfolio from "./components/Portfolio";
 import Contact from "./components/Contact";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
       <About />
       <Portfolio />
       <Contact />
+      <Footer />
     </div>
   );
 };
