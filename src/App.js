636369f9333dc11962bf9d2e88b63f6ac937104c
Wilson Chen <wilsonchen901@gmail.com>
2023-08-24 14:18:21 -0400

    contact me

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import About from "./components/About";
 import Profile from "./components/Profile";
 import Portfolio from "./components/Portfolio";
+import Contact from "./components/Contact";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Profile />
       <About />
       <Portfolio />
+      <Contact />
     </div>
   );
 };
