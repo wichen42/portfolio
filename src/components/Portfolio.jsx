b8626096084ee4c704f3b11ca52a471a0bfd868d
Wilson Chen <wilsonchen901@gmail.com>
2023-09-06 17:46:42 -0400

    edit portfolio images

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,10 +1,20 @@
-import aviquest_ss from "../assets/aviquest.png";
-import cloundtunes_ss from "../assets/cloudtunes.png";
-import messenger_ss from "../assets/messenger.png";
+import aviquest_ss from "../assets/aviques_ss.png";
+import cloundtunes_ss from "../assets/cloudtunes_ss.png";
+import messenger_ss from "../assets/srm.gif";
+import project_mvmt_ss from "../assets/project_movement_studio.png";
 import Project from "./Project";
 
 
 const Portfolio = () => {
+    const prjct_mvmt = {
+        title: "Project Movement Studio",
+        image: project_mvmt_ss,
+        github: "https://github.com/wichen42/Project-Movement-Studio",
+        demo: "https://project-movement-studio.onrender.com/",
+        text: "Project Movement Studio is a buisness website for a fitness boutique based in New York City.",
+        techs: ["React", "SaSS", "Render"],
+    };
+
     const cloudtunes= {
         title: "Cloud Tunes",
         image: cloundtunes_ss,
@@ -38,6 +48,7 @@ const Portfolio = () => {
             <h2>PORTFOLIO</h2>
             <span>My Web Development Journey, one project at a time.</span>
         </div>
+        <Project title={prjct_mvmt.title} image={prjct_mvmt.image} github={prjct_mvmt.github} demo={prjct_mvmt.demo} text={prjct_mvmt.text} techs={prjct_mvmt.techs}/>
         <Project title={cloudtunes.title} image={cloudtunes.image} github={cloudtunes.github} demo={cloudtunes.demo} text={cloudtunes.text} techs={cloudtunes.techs}/>
         <Project title={aviquest.title} image={aviquest.image} github={aviquest.github} demo={aviquest.demo} text={aviquest.text} techs={aviquest.techs}/>
         <Project title={messenger.title} image={messenger.image} github={messenger.github} demo={messenger.demo} text={messenger.text} techs={messenger.techs}/>
